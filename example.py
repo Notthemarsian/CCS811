9224a3fa309db3f3ad4c4676ef3c082a72d158e9
Patrick Müller <Notthemarsian@users.noreply.github.com>
2018-01-25 21:49:22 +0100

    Add files via upload

---
+++ example.py
@@ -0,0 +1,20 @@
+"""Example usage basic driver CCS811.py"""
+
+from machine import Pin, I2C
+import time
+import CCS811
+
+
+
+
+
+def main():
+    i2c = I2C(scl=Pin(5), sda=Pin(4))
+    s = CCS811.CCS811(i2c)
+    time.sleep(1)
+    while True:
+        if s.data_ready():
+            print('eCO2: %d ppm, TVOC: %d ppb' % (s.eCO2, s.tVOC))
+            time.sleep(1)
+
+main()
