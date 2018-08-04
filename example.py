4f54492d421e1e6d277a1b62280825ed3efca343
Patrick Müller <Notthemarsian@users.noreply.github.com>
2018-08-04 19:22:05 +0200

    Update example.py

--- example.py
+++ example.py
@@ -10,7 +10,8 @@ import CCS811
 
 def main():
     i2c = I2C(scl=Pin(5), sda=Pin(4))
-    s = CCS811.CCS811(i2c)
+    # Adafruit sensor breakout has i2c addr: 90; Sparkfun: 91
+    s = CCS811.CCS811(i2c=i2c, addr=90)
     time.sleep(1)
     while True:
         if s.data_ready():
